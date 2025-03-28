94bffee1c436b239489540a45c3e923deebcbfe1
Jay Kroger <jaykroger2@gmail.com>
2025-03-28 13:04:33 -0400

    Added PasswordGenerator files

---
+++ maui-app/PasswordGenerator/PasswordGenerator/SettingsPage.xaml.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.Maui.Controls;
+
+namespace PasswordGenerator
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAccountTapped(object sender, EventArgs e)
+        {
+            AccountDetails.IsVisible = !AccountDetails.IsVisible;
+        }
+
+        private void OnPasswordTapped(object sender, EventArgs e)
+        {
+            PasswordDetails.IsVisible = !PasswordDetails.IsVisible;
+        }
+
+        private void OnAboutTapped(object sender, EventArgs e)
+        {
+            AboutDetails.IsVisible = !AboutDetails.IsVisible;
+        }
+
+        private void OnDarkModeClicked(object sender, EventArgs e)
+        {
+            var current = Application.Current.UserAppTheme;
+            Application.Current.UserAppTheme = current == AppTheme.Dark ? AppTheme.Light : AppTheme.Dark;
+        }
+
+    }
+}
